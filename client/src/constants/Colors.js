0b43ee9655a07f5c8354e1aff95ab3c49ce1ce49
jmbamo <58489237+JerryMbamo@users.noreply.github.com>
2022-04-15 21:39:17 -0400

    Add files via upload

---
+++ client/src/constants/Colors.js
@@ -0,0 +1,10 @@
+export const COLORS = {
+  Blue: "#13688C",
+  Navy: "#0d1321",
+  Magenta: "#373B69",
+  Purple: "#708ED2",
+  LightPurple: "#3d517f",
+  white: "#ffffff",
+  green: "#4BB543",
+  lightGrey: "#D3D3D3",
+};
