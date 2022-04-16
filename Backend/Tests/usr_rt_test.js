b9b753a2cc5acb221670fcac64fcfe99c0fdd95f
jmbamo <58489237+JerryMbamo@users.noreply.github.com>
2022-04-15 21:34:44 -0400

    Add files via upload

---
+++ Backend/Tests/usr_rt_test.js
@@ -0,0 +1,10 @@
+import User from '../Models/User';
+
+let user = new User({
+    firstName: "firstname1",
+    lastName: "lastname1",
+    email: "email1",
+    username: "user1",
+    password: "abc123",
+});
+
